f09ccf6e4246c901b915ed3eb7052b4e0679fbe6
MuthonduG <muthondugithinji@gmail.com>
2024-12-19 11:07:23 -0500

    updates: update for store to load images from models && add actual images to products model

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -113,5 +113,7 @@ STATICFILES_DIRS = [
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
